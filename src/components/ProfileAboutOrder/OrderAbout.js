691f9feea140c4254282e970e0685636c8ff37d3
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-04-28 11:07:25 +0500

    Deploy

--- src/components/ProfileAboutOrder/OrderAbout.js
+++ src/components/ProfileAboutOrder/OrderAbout.js
@@ -4,8 +4,6 @@ import React from "react";
 import classNames from "classnames";
 import {makeStyles} from "@material-ui/core/styles";
 import styles from "assets/jss/material-kit-react/components/profileAboutOrder.js";
-import Button from "../CustomButtons/Button";
-import {Link} from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 // core components
 const dashboardRoutes = [];
@@ -15,18 +13,18 @@ const useStyles = makeStyles(styles);
 export default function ProfileAboutOrder(props) {
     const classes = useStyles();
     const {name} = props.product;
+    const {desc} = props.product;
 
     return (
         <div className={classNames(classes.main)}>
             <h3 className={classes.title}>{name}</h3>
             <p className={classNames(classes.description)}>
-                Парсер позволяет собирать: ссылки, населенный пункт, заголовок, описание, имя продавца и стоимость.
-                Собираются данные организаций и юридических лиц. Для многопоточной работы программы необходимо отдельно
-                приобрести прокси: IP4 прокси (https или socks) Для разгадывания капчи нужен ключ от сервиса
-                распознавания. Поддерживается Рукапча.
+                {desc}
             </p>
             <Typography variant="subtitle1" color="textPrimary" component="p">
-                Стоимость заказа: от <span style={{fontWeight: "bold", color: "#000", textAlign: "center"}}>{200}{"₽"}</span> до <span style={{fontWeight: "bold", color: "#000", textAlign: "center"}}>{300}{"₽"}</span>
+                Стоимость заказа: от <span
+                style={{fontWeight: "bold", color: "#000", textAlign: "center"}}>{200}{"₽"}</span> до <span
+                style={{fontWeight: "bold", color: "#000", textAlign: "center"}}>{300}{"₽"}</span>
             </Typography>
             <Typography variant="subtitle1" color="textPrimary" component="p">
                 Кол-во дней на выполнение заказа: <span
